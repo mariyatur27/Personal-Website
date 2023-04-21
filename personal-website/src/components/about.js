ec6c190b9c81a8964b3062f77a6952ac302c4b61
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2023-04-21 14:20:44 -0400

    Update about.js

--- personal-website/src/components/about.js
+++ personal-website/src/components/about.js
@@ -7,7 +7,7 @@ export default function About() {
         <div className='glued'>
             <div className='container'>
                 <img src='https://homeworkhubtutoring.org/assets/img/team/me.jpg' className='about-img'/><br></br>
-                <a href= 'data/assets/resume.pdf' className='resume-btn '><button id='primary-btn'>Resume</button></a>
+                <a href= '/assets/resume.pdf' className='resume-btn' target='_blank'><button id='primary-btn'>Resume</button></a>
             </div>
             <div className='container'>
                 <p>Hey, nice to meet you! My name is Mariya and I am an incoming software engineering students at the University of Waterloo. I like everything related to tech: AI, machine learning, alogrithms, software development, you name it! One of my biggest hobbies is web development, as you can probably tell by looking at this website. Even though I am just starting university, I have already managed to get 1-2 years of work experience in the tech industry, and I am always open to new work and project opportunities that will help me further perfect my skills, gain more experience, venture into the unknown and meet cool people.</p>
