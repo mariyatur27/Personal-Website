a4396a3fd0a8e970554f484644de816a46cf29f1
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2023-04-21 19:15:23 -0400

    Updates

---
+++ precache-manifest.19be7cc7349a94c1e86a7ff23ff49f63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d2978be2d2f9507c346",
+    "url": "/static/css/main.542f7ff8.chunk.css"
+  },
+  {
+    "revision": "2d2978be2d2f9507c346",
+    "url": "/static/js/main.2d2978be.chunk.js"
+  },
+  {
+    "revision": "4b28acbbe31d45ed0eac",
+    "url": "/static/js/1.4b28acbb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0d8cf754c4ad54a507e58bcac683505",
+    "url": "/index.html"
+  }
+];
