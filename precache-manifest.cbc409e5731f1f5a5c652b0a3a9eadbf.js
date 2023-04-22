306c55f579debea084bf8e285970794421a00545
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2023-04-22 15:14:37 -0400

    Updates

---
+++ precache-manifest.cbc409e5731f1f5a5c652b0a3a9eadbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d2978be2d2f9507c346",
+    "url": "/Personal-Website/static/css/main.542f7ff8.chunk.css"
+  },
+  {
+    "revision": "2d2978be2d2f9507c346",
+    "url": "/Personal-Website/static/js/main.2d2978be.chunk.js"
+  },
+  {
+    "revision": "4b28acbbe31d45ed0eac",
+    "url": "/Personal-Website/static/js/1.4b28acbb.chunk.js"
+  },
+  {
+    "revision": "87f34ce215affaea4209",
+    "url": "/Personal-Website/static/js/runtime~main.87f34ce2.js"
+  },
+  {
+    "revision": "3d02b8e176a0400778f395e5e7369456",
+    "url": "/Personal-Website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19be7cc7349a94c1e86a7ff23ff49f63.js"
+  "/Personal-Website/precache-manifest.cbc409e5731f1f5a5c652b0a3a9eadbf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Personal-Website/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
