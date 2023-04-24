f661b87f573f23c1b24d271dd4590830602460ab
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2023-04-24 18:46:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <>
       <Navbar />
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path='/'
               element={
