0cb709b630649c03333e2df4091441fce5a2680a
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2023-04-30 17:42:09 -0400

    fixing others page

--- src/components/blog.js
+++ src/components/blog.js
@@ -4,11 +4,11 @@ export default function Blog() {
   return (
     <section id='blog' className='center-sec-seciton'>
         <h2 className='section-title'>Blog...</h2>
-        <div className='blog-container'>
+        <div className='blog-container-2'>
             <img src={`${process.env.PUBLIC_URL}/assets/light-icon.png`} className='blog-img'/>
             <h2>Sometimes I write.</h2>
             <p>I am not a writer, but I like to find ways to share my opinions, ideas and experiences with others. So, this "blog" is a way for me to do so. In this blog, I will be talking about my high school and university experiences, student life and personal topics. If you have a few minutes to spare, you should definetly try reading some of it, I am sure you will find something that you like :)</p>
-            <a href='/blog'><button className='secondary-btn'>Read Something</button></a>
+            <a href={`${process.env.PUBLIC_URL}/blog`}><button className='secondary-btn'>Read Something</button></a>
         </div>
     </section>
   )
--- src/components/other.js
+++ src/components/other.js
@@ -3,23 +3,23 @@ import React from 'react'
 export default function Other() {
   return (
     <section id='other' className='center-sec-seciton'>
-        <h2 className='section-title'>Other Stuff...</h2>
+        {/* <h2 className='section-title'>Other Stuff...</h2> */}
         <div className='other-container'>
             <div className='box'>
-                <img src={`${process.env.PUBLIC_URL}/assets/instagram-icon.png`} />
-                <a href='https://www.instagram.com/mariyaaa_turetska_/' target='_blank'><h4>Follow me on Instagram.</h4></a>
+                {/* <img src={`${process.env.PUBLIC_URL}/assets/instagram-icon.png`} /> */}
+                <a href='https://www.instagram.com/mariyaaa_turetska_/' target='_blank'><h4> --> Follow me on Instagram.</h4></a>
             </div>
             <div className='box'>
-                <img src={`${process.env.PUBLIC_URL}/assets/linkedin-icon.png`} />
-                <a href='https://www.linkedin.com/in/mariya-turetska-36901a1b0/' target='_blank'><h4>Connect with me on LinkedIn.</h4></a>
+                {/* <img src={`${process.env.PUBLIC_URL}/assets/linkedin-icon.png`} /> */}
+                <a href='https://www.linkedin.com/in/mariya-turetska-36901a1b0/' target='_blank'><h4> --> Connect with me on LinkedIn.</h4></a>
             </div>
             <div className='box'>
-                <img src={`${process.env.PUBLIC_URL}/assets/gmail-icon.png`} />
-                <a href='mailto:mariyatur27@gmail.com' target='_blank'><h4>Send me an email.</h4></a>
+                {/* <img src={`${process.env.PUBLIC_URL}/assets/gmail-icon.png`} /> */}
+                <a href='mailto:mariyatur27@gmail.com' target='_blank'><h4> --> Send me an email.</h4></a>
             </div>
             <div className='box'>
-                <img src={`${process.env.PUBLIC_URL}/assets/github-icon.png`} />
-                <a href='https://github.com/mariyatur27' target='_blank'><h4>See my code on GitHub.</h4></a>
+                {/* <img src={`${process.env.PUBLIC_URL}/assets/github-icon.png`} /> */}
+                <a href='https://github.com/mariyatur27' target='_blank'><h4> --> See my code on GitHub.</h4></a>
             </div>
         </div>
     </section>
