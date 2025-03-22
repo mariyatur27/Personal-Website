6eeb10ae9d79c26db56f97473e9728b590b2b70f
mariyatur27 <mariyatur27@gmail.com>
2025-03-22 18:39:18 -0400

    latest changes

--- src/components/about.js
+++ src/components/about.js
@@ -6,12 +6,12 @@ export default function About() {
         <h2 className='section-title'>About me...</h2>
         <div className='glued'>
             <div className='container'>
-                <img src='https://homeworkhubtutoring.org/assets/img/team/me.jpg' className='about-img'/><br></br>
+                <img src='/assets/headshot.jpg' className='about-img'/><br></br>
                 
             </div>
             <div className='container'>
                 <p>Hey, nice to meet you! My name is Mariya and I am a software engineering Co-op student at the University of Waterloo. I like everything related to tech: AI, machine learning, alogrithms, software development, you name it! One of my biggest hobbies is web development, as you can probably tell by looking at this website. I have 2-3 years of work experience in the tech industry, and I am always open to new work and project opportunities that will help me further perfect my skills, gain more experience, venture into the unknown and meet cool people.</p>
-                <a href= {`${process.env.PUBLIC_URL}/assets/winter2025_Resume.pdf`} className='resume-btn' target='_blank'><button id='primary-btn'>Resume</button></a>
+                <a href= {`${process.env.PUBLIC_URL}/assets/fall2025_Resume.pdf`} className='resume-btn' target='_blank'><button id='primary-btn'>Resume</button></a>
             </div>
         </div>
     </section>
