65c09a6c02b00ba071ce389d501c3e215892cffb
mars273 <mturetsk@uwaterloo.ca>
2025-07-25 10:10:20 -0400

    latest

--- src/components/about.js
+++ src/components/about.js
@@ -10,7 +10,7 @@ export default function About() {
                 
             </div>
             <div className='container'>
-                <p>Hey, nice to meet you! My name is Mariya and I am a software engineering Co-op student at the University of Waterloo. I like everything related to tech: AI, machine learning, alogrithms, software development, you name it! One of my biggest hobbies is web development, as you can probably tell by looking at this website. I have 2-3 years of work experience in the tech industry, and I am always open to new work and project opportunities that will help me further perfect my skills, gain more experience, venture into the unknown and meet cool people.</p>
+                <p>Hey, nice to meet you! My name is Mariya and I am a software engineering student at the University of Waterloo. I like everything related to tech: AI, machine learning, alogrithms, software development, you name it! One of my biggest hobbies is web development, as you can probably tell by looking at this website. I have 2-3 years of work experience in the tech industry, and I am always open to new work and project opportunities that will help me further perfect my skills, gain more experience, venture into the unknown and meet cool people.</p>
                 <a href= {`${process.env.PUBLIC_URL}/assets/fall2025_Resume.pdf`} className='resume-btn' target='_blank'><button id='primary-btn'>Resume</button></a>
             </div>
         </div>
